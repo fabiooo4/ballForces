4d7d1b3c324c39eba4ea7d633bf1d5d4e8306bf4
fabiooo4 <fabioirimie@gmail.com>
2022-12-04 21:26:32 +0100

    added applyForce function with gravity and wind forces

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 class Ball {
-  constructor(pos, vel, acc, size) {
+  constructor(pos, size) {
     this.pos = pos;
-    this.vel = vel;
-    this.acc = acc;
+    this.vel = createVector(0,0);
+    this.acc = createVector(0,0);
     this.size = size;
   }
 
@@ -13,23 +13,28 @@ class Ball {
   }
 
   update() {
-    // Move towards mouse
-    let mouse = createVector(mouseX, mouseY);
-    mouse = mouse.sub(this.pos);
-    mouse = mouse.setMag(0.5)
-    
-    this.acc = mouse
-
+    // Increment velocity based on acceleration
     this.vel.add(this.acc);
+    // Increment position based on velocity
     this.pos.add(this.vel);
 
-    this.vel.limit(10);
+    // Limit velocity
+    // this.vel.limit(10);
+
+    this.acc = this.acc.mult(0);
+  }
+
+  applyForce(force) {
+    this.acc = force;
   }
 
   bounce() {
+    // Check lateral borders
     if (this.pos.x > width - this.size || this.pos.x < 0 + this.size) {
       this.vel.x *= -1;
     }
+
+    // Check vertical borders
     if (this.pos.y > height - this.size || this.pos.y < 0 + this.size) {
       this.vel.y *= -1;
     }
@@ -42,24 +47,30 @@ function setup() {
   createCanvas(500, 500);
 
   let pos = createVector(width/2, height/2);
-  let vel = createVector(0, 6);
-  let acc = createVector(0, 0.1);
 
   // Create 1 ball and push it into the array
-  let ball = new Ball(pos, vel, acc, 25);
-  balls.push(ball);
-
-  
+  let ball = new Ball(pos, 25);
+  balls.push(ball);  
 }
 
 function draw() {
-  background(0, 50);
+  background(0, 10);
+
+  let gravity = createVector(0, 0.5);
+  let wind = createVector(0.3, 0);
 
   // Draw ball and apply movement
   for (const ball of balls) {
     ball.draw();
     ball.update();
     ball.bounce();
+
+    // Apply forces
+    ball.applyForce(gravity);
+
+    if (mouseIsPressed) {
+      ball.applyForce(wind);
+    }
   }
   
 }
