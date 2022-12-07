5292d72b6569db053a07df4eae804cf5ae9796ae
fabiooo4 <fabioirimie@gmail.com>
2022-12-07 19:22:07 +0100

    added mass to force formula

--- sketch.js
+++ sketch.js
@@ -1,15 +1,51 @@
+let balls = [];
+
+function setup() {
+  createCanvas(500, 500);
+
+  // Create i balls and push them into the array
+  for (let i = 0; i < 3; i++) {
+    let ball = new Ball(random(0, width), random(0, height/2), random(1,3));
+    balls.push(ball); 
+  }
+}
+
+function draw() {
+  background(0);
+
+  // Draw ball and apply movement
+  for (const ball of balls) {
+    // Define forces
+    let gravity = createVector(0, 0.6 * ball.mass);
+    let wind = createVector(0.3, 0);
+
+    // Apply forces
+    ball.applyForce(gravity);
+
+    if (mouseIsPressed) {
+      ball.applyForce(wind);
+    }
+
+    ball.draw();
+    ball.update();
+    ball.bounce();
+  }
+  
+}
+
+
 class Ball {
-  constructor(pos, mass) {
-    this.pos = pos;
+  constructor(x, y, mass) {
+    this.pos = createVector(x, y);
     this.vel = createVector(0,0);
     this.acc = createVector(0,0);
     this.mass = mass;
   }
 
   draw() {
-    fill(255);
+    fill(255, 40);
     stroke(255);
-    ellipse(this.pos.x, this.pos.y, this.mass);
+    ellipse(this.pos.x, this.pos.y, this.mass*20);
   }
 
   update() {
@@ -18,61 +54,32 @@ class Ball {
     // Increment position based on velocity
     this.pos.add(this.vel);
 
-    // Limit velocity
-    // this.vel.limit(10);
-
+    // Reset acceleration for the next frame
     this.acc = this.acc.mult(0);
   }
 
   applyForce(force) {
-    this.acc.add(force);
+    let f = p5.Vector.div(force, this.mass)
+    this.acc.add(f);
   }
 
   bounce() {
     // Check lateral borders
-    if (this.pos.x > width - this.mass/2 || this.pos.x < 0 + this.mass/2) {
-      this.vel.x *= -1;
-      this.pos.x = width - this.mass/2;
+    if (this.pos.x > width - this.mass*10) {
+      this.vel.x *= -0.9;
+      this.pos.x = width - this.mass*10;
+    } else if (this.pos.x < 0 + this.mass*10) {
+      this.vel.x *= -0.9;
+      this.pos.x = 0 + this.mass*10;
     }
 
     // Check vertical borders
-    if (this.pos.y > height - this.mass/2 || this.pos.y < 0 + this.mass/2) {
-      this.vel.y *= -1;
-      this.pos.y = height - this.mass/2;
+    if (this.pos.y > height - this.mass*10) {
+      this.vel.y *= -0.9;
+      this.pos.y = height - this.mass*10;
+    } else if (this.pos.y < 0 + this.mass*10) {
+      this.vel.y *= -0.9;
+      this.pos.y = 0 + this.mass*10;
     }
   }
 } 
-
-let balls = [];
-
-function setup() {
-  createCanvas(500, 500);
-
-  let pos = createVector(width/2, height/2);
-
-  // Create 1 ball and push it into the array
-  let ball = new Ball(pos, 25);
-  balls.push(ball);  
-}
-
-function draw() {
-  background(0, 10);
-
-  let gravity = createVector(0, 1);
-  let wind = createVector(0.3, 0);
-
-  // Draw ball and apply movement
-  for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.bounce();
-
-    // Apply forces
-    ball.applyForce(gravity);
-
-    if (mouseIsPressed) {
-      ball.applyForce(wind);
-    }
-  }
-  
-}
